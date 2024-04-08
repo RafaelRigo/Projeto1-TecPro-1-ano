3ee181bcd218f0d7c3efde90c17462850b639c8d
RafaelRigo <rigo.f.rafa@gmail.com>
2024-04-08 09:47:04 -0300

    começo

---
+++ obra.py
@@ -0,0 +1,8 @@
+class Obra:
+    def __init__(self) -> None:
+        self.anoDaObra = ""
+        self.mesDaObra = ""
+        self.autorDaObra = ""
+        self.nomeDaObra = ""
+        self.estilo = ""
+        self.valorEstimado = ""
