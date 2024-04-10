21622ed88afe915de63447a2c061c841ce38c6d1
RafaelRigo <rigo.f.rafa@gmail.com>
2024-04-10 17:42:58 -0300

    classe obra, seletor e cadastro de obras

---
+++ main.py
@@ -0,0 +1,85 @@
+import os, tkinter as tk, time
+from tkinter import filedialog
+from obra import Obra
+
+clear = lambda:os.system('cls') or None
+
+root = tk.Tk()
+root.withdraw()
+root.attributes("-topmost", True)
+
+def selecionar_arquivo():
+    tipos_arq = (
+        ('Texto', '*.txt'),
+        ('HTML', '*.html'),
+        ('Outros', '*.*')
+    )
+
+    arq = filedialog.askopenfilename(title='Escolha o arquivo que contém as obras',
+                                          initialdir='C:',
+                                          multiple = False,
+                                          filetypes=tipos_arq)
+    
+    return arq
+
+def cadastro_de_obras():
+    root.update()
+
+    nome_arq = selecionar_arquivo()
+    obra = Obra(nome_arq, True)
+
+    ano = ''
+    while ano != '0':
+        ano = input('Digite o ano da obra ("0" para sair e registrar as obras) -> ')
+        if ano != '0':
+            mes = input('Digite o mês da obra -> ')
+            estilo = input('Digite o estilo da obra -> ')
+            nome_obra = input('Digite o nome da obra -> ')
+            nome_autor = input('Digite o nome do autor -> ')
+            valor = input('Digite o valor estimado da obra -> ')
+            url_foto = input('Digite o URL da foto da obra -> ')
+
+            obra.preencherCampos(ano, mes, estilo, nome_obra, nome_autor, valor, url_foto)
+            obra.gravarCamposNoArquivo()
+
+            clear()
+    obra.fecharArquivo()
+    print('Suas obras foram registradas com êxito!')  
+    time.sleep(5)
+    
+def listagem_de_obras():
+    pass
+
+def pagina_web_de_obras():
+    pass
+
+def triangulo_de_pascal():
+    pass
+
+def menu():
+    clear()
+    print('>=-•-•-•-•-•-=< MENU >=-•-•-•-•-•-=<')
+    print('|1| - Cadastro de obras de arte')
+    print('|2| - Listagem de obras de arte')
+    print('|3| - Página Web de obras de arte')
+    print('|4| - Triangulo de Pascal')
+    print('|0| - Sair')
+    print('>=-•-•-•-•-•-•-•-•-•-•--•-•-•-•-•-=<')
+
+def seletor():
+    continuar = lambda:input('\nDeseja continuar? [ENTER] -> ')
+    opcao = ''
+    while opcao != '0':
+        menu()
+        opcao = input('Digite a opção desejada -> ')
+        clear()
+        match opcao:
+            case '1': cadastro_de_obras(), continuar()
+            case '2': listagem_de_obras(), continuar()
+            case '3': pagina_web_de_obras(), continuar()
+            case '4': triangulo_de_pascal(), continuar()
+    
+    print("\nObrigado pelo uso, até mais!\n")
+
+if __name__ == "__main__":
+    seletor()
--- obra.py
+++ obra.py
@@ -1,8 +1,75 @@
 class Obra:
-    def __init__(self) -> None:
+    def __init__(self, nomeArq: str, aberto: bool) -> None:
         self.anoDaObra = ""
         self.mesDaObra = ""
-        self.autorDaObra = ""
-        self.nomeDaObra = ""
         self.estilo = ""
+        self.nomeDaObra = ""
+        self.autorDaObra = ""
         self.valorEstimado = ""
+        self.urlFoto = ""
+
+        self._abertoParaGravacao = aberto
+        if self._abertoParaGravacao:
+            self._arquivo = open(nomeArq, 'a', encoding='utf-8')
+        else:
+            self._arquivo = open(nomeArq, 'r', encoding='utf-8')
+    
+    def lerCamposDoArquivo(self) -> None:
+        if not self._abertoParaGravacao:
+            linha = self._arquivo.readline()
+            self.anoDaObra = linha[:4].rstrip()
+            self.mesDaObra = linha[4:6].rstrip()
+            self.estilo = linha[6:21].rstrip()
+            self.nomeDaObra = linha[21:41].rstrip()
+            self.autorDaObra = linha[41:61].rstrip()
+            self.valorEstimado = float(linha[61:71].rstrip())
+            self.urlFoto = linha[71:171].rstrip()
+
+    def gravarCamposNoArquivo(self) -> None:
+        if self._abertoParaGravacao:
+            self._arquivo.writelines(
+                self.anoDaObra.ljust(4) + 
+                self.mesDaObra.ljust(2) + 
+                self.estilo.ljust(15) +
+                self.nomeDaObra.ljust(20) + 
+                self.autorDaObra.ljust(20) + 
+                self.valorEstimado.ljust(15) +
+                self.urlFoto.ljust(100) + '\n'
+                )
+
+    def preencherCampos(self, novoAno: str, novoMes: str, novoEstilo: str, novoNome: str, nomeAutor: str, novoValor: str, novoURL: str) -> None:
+        self.anoDaObra = novoAno
+        self.mesDaObra = novoMes
+        self.estilo = novoEstilo
+        self.nomeDaObra = novoNome
+        self.autorDaObra = nomeAutor
+        self.valorEstimado = novoValor
+        self.urlFoto = novoURL
+
+    def fecharArquivo(self) -> None:
+        self._arquivo.close()
+
+    def __str__(self) -> str:
+        return f"{
+            self.anoDaObra.ljust(4) + 
+            self.mesDaObra.ljust(2) + 
+            self.estilo.ljust(15) +
+            self.nomeDaObra.ljust(20) + 
+            self.autorDaObra.ljust(20) + 
+            str(self.valorEstimado).ljust(15) +
+            self.urlFoto.ljust(100)
+            }"
+
+    def compararCom(self, outraObra) -> int:
+        if not isinstance(outraObra, Obra): # verifica se o parâmetro 'outraObra' é uma instância da classe 'Obra'
+            raise TypeError("O parâmetro deve ser instância da classe \"Obra\"")
+
+        inst_atual = self.anoDaObra + self.mesDaObra + self.autorDaObra + self.nomeDaObra
+        outra_inst = outraObra.anoDaObra + outraObra.mesDaObra + outraObra.autorDaObra + outraObra.nomeDaObra
+
+        if len(inst_atual) < len(outra_inst):
+            return -1
+        elif len(inst_atual) == len(outra_inst):
+            return 0
+        elif len(inst_atual) > len(outra_inst):
+            return 1
